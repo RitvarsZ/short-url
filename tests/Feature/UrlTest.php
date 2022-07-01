409bbe8c3257abb604b743ae70a07ec335ec0ba1
RitvarsZ <ritvarszvejnieks@protonmail.com>
2022-07-01 19:44:59 +0300

    Add feature test

---
+++ tests/Feature/UrlTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Url;
+
+class UrlTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test for landing on the homepage.
+     *
+     * @return void
+     */
+    public function test_homepage_loads()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test a valid url.
+     *
+     * @return void
+     */
+    public function test_valid_url_can_be_turned_into_short_url()
+    {
+        $response = $this->post('/create', [
+            'long_url' => 'https://google.com/'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertViewIs('success');
+    }
+
+    /**
+     * Test a invalid url.
+     *
+     * @return void
+     */
+    public function test_invalid_url_returns_an_error()
+    {
+        $response = $this->post('/create', [
+            'long_url' => 'asd'
+        ]);
+
+        $response->assertRedirect('/');
+        $response->assertInvalid(['long_url']);
+    }
+
+    /**
+     * Test if existing url gets returned if it had already been shortened.
+     *
+     * @return void
+     */
+    public function test_existing_url_gets_returned()
+    {
+        $url = Url::factory()->create([
+            'full_url' => 'https://google.com'
+        ]);
+
+        $shortUrl = url('/'.$url->hash);
+
+        $response = $this->post('/create', [
+            'long_url' => 'https://google.com'
+        ]);
+
+        $this->assertEquals($shortUrl, $response['shortUrl']);
+    }
+}
